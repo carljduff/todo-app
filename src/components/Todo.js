4dcf1942c10fd707b20fd18f27b9fd7edc0a8d4c
Carl Jordan Duff <jordan.duff@student.awesomeinc.org>
2022-03-22 19:02:59 +0000

    created statusbtn

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,19 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import '../App.css'
+
 export default function Todo() {
     return(
         <>
-       
-       <input type='checkbox' />
+
+       <div className='todo-wrap'>
+          
+
+            <input type='checkbox' />
+            <h4 className="todo"> THIS IS A TODO</h4>
+            <button className='button'>X</button>
+           
+        </div>
+
         </>
     )
 }
